4989587faa0842c11a93b0e620ce1399ac067042
Anto Prijosoesilo <2231444+antogg@users.noreply.github.com>
2025-03-25 09:57:46 +0700

    Conditionals

---
+++ cond.py
@@ -0,0 +1,34 @@
+# conditionals
+if True:
+    ...
+else:
+    ...
+
+if True:
+    ...
+elif False:
+    ...
+elif 1==2:
+    ...
+else:
+    ...
+
+# loops
+# for i in "abcdefg":
+#     print(i)
+
+# for j in range(0, 10, 2):
+#     print(j)
+
+x = 7
+c = 0
+# while True:
+#     if c >= x:
+#         break
+#     print(c)
+#     c = c+1
+
+while c < x:
+    print(c)
+    # c = c+1
+    c += 1
